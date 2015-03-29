906509050d08ae3af4ac9a8b59de9d2d143c0f61
michaelliang0709 <michaelliang0709@gmail.com>
2015-03-29 18:29:40 -0400

    Create users.sql

---
+++ users.sql
@@ -0,0 +1,5 @@
+INSERT INTO `users` (`user_id`,`username`,`firstname`,`lastname`,`password`,`usertype`,`created_date`) VALUES (1,'luke','Luke','Skywalker',sha1('ekul'),'standard','2015-03-01 17:51:23');
+INSERT INTO `users` (`user_id`,`username`,`firstname`,`lastname`,`password`,`usertype`,`created_date`) VALUES (2,'leia','Leia','Organa',sha1('aiel'),'standard','2015-03-01 17:51:23');
+INSERT INTO `users` (`user_id`,`username`,`firstname`,`lastname`,`password`,`usertype`,`created_date`) VALUES (3,'vader','Darth','Vader',sha1('redav'),'standard','2015-03-01 17:51:23');
+INSERT INTO `users` (`user_id`,`username`,`firstname`,`lastname`,`password`,`usertype`,`created_date`) VALUES (4,'ben','Ben','Kenobi',sha1('neb'),'admin','2015-03-01 17:51:23');
+INSERT INTO `users` (`user_id`,`username`,`firstname`,`lastname`,`password`,`usertype`,`created_date`) VALUES (5,'han','Han','Solo',sha1('nah'),'standard','2015-03-01 18:11:45');

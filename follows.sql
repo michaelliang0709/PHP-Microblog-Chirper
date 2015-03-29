4d4a7c1baab3f09cc4f6c90305a8c908d0e1fc21
michaelliang0709 <michaelliang0709@gmail.com>
2015-03-29 18:29:00 -0400

    Create follows.sql

---
+++ follows.sql
@@ -0,0 +1,8 @@
+INSERT INTO `follows` (`user_id`,`follows_id`,`created_date`) VALUES (1,2,'2015-03-01 17:57:10');
+INSERT INTO `follows` (`user_id`,`follows_id`,`created_date`) VALUES (1,4,'2015-03-01 17:57:10');
+INSERT INTO `follows` (`user_id`,`follows_id`,`created_date`) VALUES (2,3,'2015-03-01 17:57:10');
+INSERT INTO `follows` (`user_id`,`follows_id`,`created_date`) VALUES (2,4,'2015-03-01 17:57:11');
+INSERT INTO `follows` (`user_id`,`follows_id`,`created_date`) VALUES (3,1,'2015-03-01 17:57:11');
+INSERT INTO `follows` (`user_id`,`follows_id`,`created_date`) VALUES (3,2,'2015-03-01 17:57:11');
+INSERT INTO `follows` (`user_id`,`follows_id`,`created_date`) VALUES (3,4,'2015-03-01 17:57:11');
+INSERT INTO `follows` (`user_id`,`follows_id`,`created_date`) VALUES (4,1,'2015-03-01 17:57:10');
